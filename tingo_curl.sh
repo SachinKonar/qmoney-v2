ae78c7598a447603d8b191e7f385c8e9683c14b8
Sam Konar <132926747+SachinKonar@users.noreply.github.com>
2025-09-03 14:56:30 -0400

    The corrected unzipped files
    The corrected unzipped files

---
+++ tingo_curl.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#!/bin/bash
+
+curl "https://api.tiingo.com/tiingo/daily/mcd/prices/?token=2f67393bdd2faf29ebbc2c5abe0570deb737dd82"
+curl "https://api.tiingo.com/tiingo/daily/mos/prices/?token=2f67393bdd2faf29ebbc2c5abe0570deb737dd82"
+curl "https://api.tiingo.com/tiingo/daily/PRDSY/prices/?token=2f67393bdd2faf29ebbc2c5abe0570deb737dd82"
